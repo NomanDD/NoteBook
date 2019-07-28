cf61bcfe912c0730e3a126c084d9f531314345e7
NomanDD <scout22noman@gmail.com>
2019-07-28 17:40:14 +0800

    Add git skill

---
+++ git_skill/basic.sh
@@ -0,0 +1,19 @@
+
+
+# 1、查看git配置信息
+git config --list
+
+
+# 2、查看git用户名
+git config user.name
+
+
+# 3、查看邮箱配置
+git config user.email
+
+# 4、全局配置用户名
+git config --global user.name "NomanDD"
+
+# 5、全局配置邮箱
+git config --global user.email "scout22noman@gmail.com"
+
